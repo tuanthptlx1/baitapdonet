7380577670a2e8f79aae2ee9d029c18c322bc5bf
tuanthptlx1 <nt7304398@gmail.com>
2024-10-24 16:26:31 +0700

    1
    1

--- noiluubaitap/frmdangnhap/frmdangnhap/obj/Debug/net8.0-windows/frmdangnhap.AssemblyInfo.cs
+++ noiluubaitap/frmdangnhap/frmdangnhap/obj/Debug/net8.0-windows/frmdangnhap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frmdangnhap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a4d9a28fd312c46ef96bf7fe630689692a7d7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f1e3cfb1e1831820d0df8b43dfb2ef96867bc2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("frmdangnhap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frmdangnhap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
