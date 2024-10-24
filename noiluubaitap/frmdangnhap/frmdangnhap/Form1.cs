44a4d9a28fd312c46ef96bf7fe630689692a7d7d
tuanthptlx1 <nt7304398@gmail.com>
2024-10-24 16:16:59 +0700

    1
    1

---
+++ noiluubaitap/frmdangnhap/frmdangnhap/Form1.cs
@@ -0,0 +1,40 @@
+﻿namespace frmdangnhap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//dang nhap
+        {
+            string username = textBox1.Text; 
+            string password = textBox2.Text;
+
+            if (username == "admin" && password == "1234")
+            {
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)//thoat
+        {
+            Application.Exit();
+        }
+    }
+}
